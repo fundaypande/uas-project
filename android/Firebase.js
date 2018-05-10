a05e10f5e5ff20d2727b74ad564245162b8d4f65
fundaypande <funday820@gmail.com>
2018-05-10 11:50:08 +0800

    first commit

---
+++ android/Firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: 'AIzaSyBDoxRIQkNd3emU8hh7pHbN9uhDynBa8pc',
+    authDomain: 'yourassignment-dc355.firebaseapp.com',
+    databaseURL: 'https://yourassignment-dc355.firebaseio.com',
+    projectId: 'yourassignment-dc355',
+    storageBucket: 'yourassignment-dc355.appspot.com',
+    messagingSenderId: '153565304323'
+  };
+
+export default config;
